9293cb7c744f015734a3f7f6e220207478db5b03
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-30 16:05:49 -0300

    aprendendo navegacao entre telas com swift

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  atm consultoria estudos
+//
+//  Created by Joao Luiz Marcelino Ribeiro on 30/08/23.
+//  Copyright © 2023 Curso IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
